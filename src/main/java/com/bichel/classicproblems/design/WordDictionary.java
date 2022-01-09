758287db307463b5faa76085f8bdf41441f16ca6
Yauhen Bichel <ybichel@eisgroup.com>
2022-01-09 17:51:03 +0000

    added classical design problems

---
+++ src/main/java/com/bichel/classicproblems/design/WordDictionary.java
@@ -0,0 +1,41 @@
+package com.bichel.classicproblems.design;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WordDictionary {
+    Map<Integer, Set<String>> d;
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        d = new HashMap();
+    }
+
+
+    public void addWord(String word) {
+        int m = word.length();
+        if (!d.containsKey(m)) {
+            d.put(m, new HashSet<>());
+        }
+
+        d.get(m).add(word);
+    }
+
+    public boolean search(String word) {
+        int m = word.length();
+
+        if (d.containsKey(m)) {
+            for (String w : d.get(m)) {
+                int i = 0;
+                while ((i < m) && (w.charAt(i) == word.charAt(i) || word.charAt(i) == '.')) {
+                    i++;
+                }
+                if (i == m) return true;
+            }
+        }
+
+        return false;
+    }
+}
