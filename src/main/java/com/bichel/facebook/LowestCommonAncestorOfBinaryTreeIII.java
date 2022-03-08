a4bfda8d390a0f05b9489777533b2f28fa1cf04f
Yauhen Bichel <ybichel@eisgroup.com>
2022-03-08 12:09:02 +0000

    added problems

--- src/main/java/com/bichel/facebook/LowestCommonAncestorOfBinaryTreeIII.java
+++ src/main/java/com/bichel/facebook/LowestCommonAncestorOfBinaryTreeIII.java
@@ -2,6 +2,22 @@ package com.bichel.facebook;
 
 import com.bichel.leetcode.graph.ParentNode;
 
+/*
+Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+
+Each node will have a reference to its parent node. The definition for Node is below:
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)."
+
+
+ */
+
 public class LowestCommonAncestorOfBinaryTreeIII {
     private boolean hasP, hasQ;
 
@@ -11,23 +27,25 @@ public class LowestCommonAncestorOfBinaryTreeIII {
 
         ParentNode root = p;
 
-        while(root.parent != null) root = root.parent;
+        while(root.parent != null)
+            root = root.parent;
 
         ParentNode lca = findLca(root, p, q);
 
-        return hasP == true && hasQ == true ? lca : null;
+        return hasP && hasQ ? lca : null;
     }
 
     private ParentNode findLca(ParentNode root, ParentNode p, ParentNode q) {
-        if(root == null) return null;
+        if(root == null)
+            return null;
 
         ParentNode leftLca = findLca(root.left, p, q);
         ParentNode rightLca = findLca(root.right, p, q);
 
-        if(hasP == false && root.val == p.val) {
+        if(!hasP && root.val == p.val) {
             hasP = true;
         }
-        if(hasQ == false && root.val == p.val) {
+        if(!hasQ && root.val == p.val) {
             hasQ = true;
         }
 
