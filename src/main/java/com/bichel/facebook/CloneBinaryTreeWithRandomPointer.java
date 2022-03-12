6dc0f4d8c1d46ae0d592f31859a8cd3b9f5bb7df
Yauhen Bichel <ybichel@eisgroup.com>
2022-03-12 08:40:04 +0000

    added problems

--- src/main/java/com/bichel/facebook/CloneBinaryTreeWithRandomPointer.java
+++ src/main/java/com/bichel/facebook/CloneBinaryTreeWithRandomPointer.java
@@ -1,7 +1,21 @@
 package com.bichel.facebook;
 
 public class CloneBinaryTreeWithRandomPointer {
-    //public NodeCopy copyRandomBinaryTree(Node root) {
+   /* private Map<Node, NodeCopy> visitedMap = new HashMap<>();
 
-    //}
+    public NodeCopy copyRandomBinaryTree(Node root) {
+        if(root == null) return null;
+        if(visitedMap.containsKey(root)) return visitedMap.get(root);
+
+        NodeCopy clonedNode = new NodeCopy(root.val);
+        visitedMap.put(root, clonedNode);
+
+        clonedNode.left = copyRandomBinaryTree(root.left);
+        clonedNode.right = copyRandomBinaryTree(root.right);
+        clonedNode.random = copyRandomBinaryTree(root.random);
+
+        return clonedNode;
+    }
+
+    */
 }
