5f46917c495f15dcb9612d478e5bc0ee65f8fe81
Yauhen Bichel <ybichel@eisgroup.com>
2022-03-15 22:32:40 +0000

    added problems

---
+++ src/main/java/com/bichel/facebook/MaxConsecutiveOnesIII.java
@@ -0,0 +1,38 @@
+package com.bichel.facebook;
+
+/*
+Given a binary array nums and an integer k,
+return the maximum number of consecutive 1's
+in the array if you can flip at most k 0's.
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,_1_,1,1,1,1,_1_]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Time Complexity: O(N)
+Space Complexity: O(1). We do not use any extra space.
+ */
+
+public class MaxConsecutiveOnesIII {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int right;
+
+        for (right = 0; right < nums.length; right++) {
+            // If we included a zero in the window we reduce the value of k.
+            // Since k is the maximum zeros allowed in a window.
+            if (nums[right] == 0) {
+                k--;
+            }
+            // A negative k denotes we have consumed all allowed flips and window has
+            // more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            if (k < 0) {
+                // If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left];
+                left++;
+            }
+        }
+        return right - left;
+    }
+}
