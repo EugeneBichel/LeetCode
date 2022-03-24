782ab5cb5fbcb92babe00b1fee4e1772e468b131
Yauhen Bichel <ybichel@eisgroup.com>
2022-03-24 16:23:58 +0000

    added reviewed problem of combinations

--- src/main/java/com/bichel/leetcode/backtracking/Combinations.java
+++ src/main/java/com/bichel/leetcode/backtracking/Combinations.java
@@ -22,49 +22,41 @@ import java.util.List;
 import java.util.LinkedList;
 
 public class Combinations {
-    private List<List<Integer>> output = new LinkedList<>();
-    private int n;
-    private int k;
-
-    private void backtrack(int first, LinkedList<Integer> curr) {
-        // if the combination is done
-        if (curr.size() == k)
-            output.add(new LinkedList<>(curr));
-
-        for (int i = first; i < n + 1; i++) {
-            // add i into the current combination
-            curr.add(i);
-            // use next integers to complete the combination
-            backtrack(i + 1, curr);
-            // backtrack
-            curr.removeLast();
-        }
-    }
-
+    /*
+    Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+    You may return the answer in any order.
+    Input: n = 4, k = 2
+    Output:
+    [
+      [2,4],
+      [3,4],
+      [2,3],
+      [1,2],
+      [1,3],
+      [1,4],
+    ]
+    */
     public List<List<Integer>> combine(int n, int k) {
-        this.n = n;
-        this.k = k;
-        backtrack(1, new LinkedList<>());
-        return output;
+        List<List<Integer>> combinations = new ArrayList<>();
+        LinkedList<Integer> combination = new LinkedList<>();
+
+        backtrack(1, n, k, combination, combinations);
+        return combinations;
     }
 
-    //other solution
-    public static List<List<Integer>> combine2(int n, int k) {
-        List<List<Integer>> combs = new ArrayList<>();
-        combine(combs, new ArrayList<>(), 1, n, k);
-        return combs;
-    }
-
-    private static void combine(List<List<Integer>> combs, List<Integer> comb,
-                                int start, int n, int k) {
-        if (k == 0) {
-            combs.add(new ArrayList<>(comb));
+    private void backtrack(int first, int n, int k, LinkedList<Integer> combination, List<List<Integer>> combinations) {
+        if (combination.size() == k) {
+            combinations.add(new LinkedList<>(combination));
+            //Solution 1 does contain 'return'. I do not understand why, but
+            //for me it looks like make sense return back to the method call
+            //and remove the last item
             return;
         }
-        for (int i = start; i <= n; i++) {
-            comb.add(i);
-            combine(combs, comb, i + 1, n, k - 1);
-            comb.remove(comb.size() - 1);
+
+        for (int i = first; i <= n; i++) {
+            combination.add(i);
+            backtrack(i + 1, n, k, combination, combinations);
+            combination.removeLast();
         }
     }
 }
