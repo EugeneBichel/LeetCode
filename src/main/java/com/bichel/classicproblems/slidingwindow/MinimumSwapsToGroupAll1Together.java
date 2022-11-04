ee214f497efc87c37313e074ea69fff39299ab14
Yauhen Bichel <yauhenbichel@yauhensookm1pro.home>
2022-11-04 15:51:38 +0000

    minor refactoring

--- src/main/java/com/bichel/classicproblems/slidingwindow/MinimumSwapsToGroupAll1Together.java
+++ src/main/java/com/bichel/classicproblems/slidingwindow/MinimumSwapsToGroupAll1Together.java
@@ -19,14 +19,28 @@ import java.util.Arrays;
 
 public class MinimumSwapsToGroupAll1Together {
     public int minSwaps(int[] data) {
-        int ones = Arrays.stream(data).sum();
+        //Input: data = [1,0,1,0,1]
+        int ones = Arrays.stream(data).sum(); //3
+
         int numOnes = 0;
         int maxOnes = 0;
+
         int left = 0;
         int right = 0;
 
+        /*
+        Input: data = [1,0,1,0,1]
+        Output: 1
+        Explanation: There are 3 ways to group all 1's together:
+        [1,1,1,0,0] using 1 swap.
+        [0,1,1,1,0] using 2 swaps.
+        [0,0,1,1,1] using 1 swap.
+        The minimum is 1.
+         */
+
         while(right < data.length) {
             numOnes += data[right++];
+
             if(right - left > ones) {
                 numOnes -= data[left++];
             }
