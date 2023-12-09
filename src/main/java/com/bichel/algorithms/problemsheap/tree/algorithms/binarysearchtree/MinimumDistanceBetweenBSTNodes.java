b4226ab3118a6efccb77da7dcecc4e46a2358fa6
Yauhen Bichel <ybichel@eisgroup.com>
2023-12-09 22:36:17 +0000

    facebook problems

--- src/main/java/com/bichel/algorithms/problemsheap/tree/algorithms/binarysearchtree/MinimumDistanceBetweenBSTNodes.java
+++ src/main/java/com/bichel/algorithms/problemsheap/tree/algorithms/binarysearchtree/MinimumDistanceBetweenBSTNodes.java
@@ -20,10 +20,38 @@ public class MinimumDistanceBetweenBSTNodes {
     }
 
     private void traverse(TreeNode root, List<Integer> items) {
-        if(root == null) return;
+        if(root == null) {
+            return;
+        }
 
         traverse(root.left, items);
         items.add(root.val);
         traverse(root.right, items);
+    }
+
+    int min = Integer.MAX_VALUE; //global variable to store ans
+    Integer preValue = null; //previous value, which we will use to compare
+
+    //When we do inorder traversal on a bst we get all values sorted in increasing order. This is the logic that will be applied here
+    public int getMinimumDifference2(TreeNode root) {
+        if(root == null) { //base condition
+            return min;
+        }
+
+        //since we are doing inorder traversal, first we are going to extreme left
+        getMinimumDifference2(root.left);
+
+        //if the preValue is not null then we subtract from the value of current node and compare with global 'min'
+        if(preValue != null) {
+            min = Math.min(min, root.val - preValue);
+        }
+
+        //we update the preValue for next node
+        preValue = root.val;
+
+        //after moving to left we move to right side
+        getMinimumDifference2(root.right);
+
+        return min;
     }
 }
